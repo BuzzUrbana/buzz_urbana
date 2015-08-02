2d56c8019fe175d4a93f4578cad959d0ec75e065
Gabe <serranof1@email.arizona.edu>
2015-08-02 15:04:02 -0700

    Remove Joke

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Caelin likes Butts"
+    render text: "BuzzUrbana!"
   end
 end
